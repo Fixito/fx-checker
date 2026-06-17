0847e7bd769d81e5b70360eb00b3a1055ff7369a
Thomas ROBERT <thomas_3004@hotmail.fr>
2026-06-17 20:38:44 +0200

    chore: add utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const fmt = {
+  amount: (num: number) =>
+    num.toLocaleString('en-US', { maximumFractionDigits: 2, minimumFractionDigits: 2 }),
+  date: (iso: string) =>
+    new Date(iso).toLocaleString('en-US', {
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      month: 'short',
+    }),
+  pct: (num: number) => `${num >= 0 ? '+' : ''}${num.toFixed(2)}%`,
+  rate: (num: number, decimals = 4) => num.toFixed(decimals),
+};
